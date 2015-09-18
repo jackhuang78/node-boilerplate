ae638480cf9aef7d4ec1098d199fe55c7bfbf434
Jack Huang <jack.huang78@gmail.com>
2015-09-18 00:53:44 -0700

    add echo test

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import ejs from 'ejs';
-
+import request from 'request';
+import url from 'url';
+import uuid from 'uuid';
 
 var app = express();
 var port = process.env.PORT || 9999;
@@ -22,11 +23,53 @@ app.use(bodyParser.json());
 app.set('views', 'src/client');
 app.set('view engine', 'ejs');
 
-
+// server is up!
 app.get('/', (req, res) => {
-	res.send('OK');
+	console.log('hey');
+	res.send('Hello World!');
 });
 
+// echoes the parameters back
+app.post('/echo/:param?', (req, res) => {
+	res.cookie('session', uuid.v4());
+	res.status(200).json({
+		params: req.params,
+		query: req.query,
+		cookie: req.cookies,
+		body: req.body,
+		headers: req.headers
+	});
+});
+
+// create a dummy request to POST /echo
+app.get('/echo/test', (req, res) => {
+	request.post({
+		url: url.format({
+			protocol: 'http', 
+			hostname: 'localhost', 
+			port: 9999, 
+			pathname: '/echo/myParam', 
+			query: {
+				q1: 'string', q2: 123, q3: true
+			}
+		}),
+		json: {
+			f1: 'field1', f2: 456, f3: false, f4: {}, f5: []
+		}
+	}, (error, response, body) => {
+		var myBody = {
+			method: response.request.method,
+			url: response.request.uri.href,
+			statusCode: response.statusCode,
+			headers: response.headers,
+			body: body
+		};
+
+		res.json(myBody);
+	});
+});
+
+// show a page with React component
 app.get('/index', (req, res) => {
 	res.render('main', {react: 'Index'});
 });
