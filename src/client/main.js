b3800283e696fa9195673b2ae4fa98fcca72681c
Jack Huang <jack.huang78@gmail.com>
2015-10-01 15:29:49 -0700

    remove comments

--- src/client/main.js
+++ src/client/main.js
@@ -12,35 +12,3 @@ var Component = components[$('#content').attr('react')];
 
 React.render(<Component/>, document.body);
 
-
-
-
-
-
-
-
-
-
-
-
-// if(false) {
-// 	React.render(<Component />, document.body);
-// } else {
-// 	Router.run(Component, Router.HistoryLocation, (Handler) => {
-//   	React.render(<Handler />, document.body);
-// 	});
-// }
-
-// React.render(
-// 	<Component />, document.body
-// );
-
-
-// React.render((<Router>
-// 		<Route path="/" component={Component}>
-// 		</Route>
-// 	</Router>), document.body);
-
-//React.render(<Router routes={{path:'/', component: Component}}></Router>, document.body);
-
-
